e3f738273ab40777b1621b3617b12d7294922121
Ali Padron <alijesus29@gmail.com>
2019-07-16 07:31:05 -0400

    Information architecture part 1.

--- themes/transeunte-theme/front-page.php
+++ themes/transeunte-theme/front-page.php
@@ -2,197 +2,100 @@
 get_header();
 while(have_posts()) { the_post(); 
 ?>
-    <!-- MENU ADENTRO -->
-    <section id="menuAdentro" class="bg-dark">
-      <!-- ECABEZADO LOGO Y BOTON MENU -->
-      <header
-        class="encabezadoMenu row d-flex container-fluid align-items-center align-content-center "
-      >
-        <div class="mascaraMenu2"></div>
-        <div class="logoMenu ml-3">
-          <a href="<?php echo site_url(); ?>">
-              <img 
-                src="<?php echo get_theme_file_uri('assets//images/icon/Transeunte-04.png')?>"
-                alt="Logo Transeúnte"
-                title="Logo Transeúnte"
-              >
-          </a>
+  <section>
+    <h1>La calle habla</h1>
+    <h2>¡Todo un viaje por la ciudad de experiencias interminables!</h2>
+    <p>Agencia de contenidos culturales</p>
+    <img
+      src="<?php echo get_theme_file_uri('assets/images/cover-page.jpg'); ?>"
+    >
+  </section>
+  <section>
+    <h2>¿Qué hacemos?</h2>
+    <h3>Literatura</h3>
+    <p>Presentaciones de libros, talleres y conversatorios.</p>
+    <h3>Música</h3>
+    <p>Mini conciertos acústicos y clínicas musicales.</p>
+    <h3>Cine y fotografía</h3>
+    <p>Cine foros, talleres, exposiciones y recorridos fotográficos.</p>
+    <h3>Artes plásticas</h3>
+    <p>Exposiciones, shows de arte en vivo y talleres.</p>
+  </section>
+  <section>
+    <h2>Orgía cultural</h2>
+  <?php
+    $homepagePosts = new WP_Query(array(
+      'post_type' => array('evento', 'contenido'),
+      'orderby' => 'date',
+    ));
+    while($homepagePosts->have_posts()) { $homepagePosts->the_post();
+      the_post_thumbnail('post-thumbnail', array(
+        'title' => get_the_title(get_post_thumbnail_id(get_the_id()))
+        ));
+  ?>
+    <img src="" alt="">
+        <div class="evento-title font-italic">
+          <span class="mx-auto"> <?php echo get_the_title(); ?> </span>
+          <div class="evento-title-verMas btn btn-light">
+            <span>Ver Más</span> <i class="fa fa-arrow-up evento-arrow" aria-hidden="true"></i>
+          </div>
         </div>
-        <div
-          class=" botonMenu  ml-auto mr-3 mr-sm-1 d-flex align-items-center justify-content-center "
+        <div class="evento-content">
+          <div class="evento-content-text">
+            <h2 class="evento-content-text-title"><?php echo get_the_title(); ?></h2>
+            <p class="evento-content-text-p"> <?php echo get_the_excerpt(); ?> </p>
+            <p class="evento-content-text-info">
+            <?php if(get_post_type() === 'evento') { ?>
+              <span><? echo get_field('lugar') ?></span>
+              <span><? echo get_field('fecha') ?></span>
+              <span><? echo get_field('tipo_de_evento') ?></span>
+              <span><? echo get_field('tipo_de_espacio') ?></span>
+              <span>Asistentes: <? echo get_field('numero_de_asistentes') ?>+</span>
+            <?php } ?>
+            <?php if(get_post_type() === 'contenido') { ?>
+              <span><? echo get_field('plataforma_o_red_social') ?></span>
+              <span>Alcance: <? echo get_field('alcance') ?></span>
+            <?php } ?>
+            </p>
+            <a class="evento-content-text-link btn btn-dark w-100 font-italic " href="<?php echo get_permalink(); ?>">
+              Ir a evento
+            </a>
+          </div>
+        </div>
+      </div>
+      <?php } wp_reset_postdata(); ?>
+    </div>
+    <!-- Eventos responsive -->
+    <div class="orgiaCultural-eventosMovil">
+    <?php
+      $homepagePosts = new WP_Query(array(
+        'post_type' => array('evento', 'contenido'),
+        'orderby' => 'date',
+      ));
+      $i = 0;
+      while($homepagePosts->have_posts()) {
+        $homepagePosts->the_post();
+        if($i === 4) { ?>
+        <div class="colapsable orgiaCultural-eventosMovil-C">
+    <?php } ?>
+      <div class="eventoMovil">
+        <a class="wow rotateInDownLeft  " href="<?php echo get_permalink(); ?>"
+          ><span class="eventoMovil-subTitle"><?php echo get_the_title(); ?></span></a
         >
-          <i class="fa fa-bars " aria-hidden="true"></i>
-        </div>
-      </header>
-      <div class="  cont-tituloMenu d-flex justify-content-sm-center  align-items-center mx-auto ">
-        <div class="copysPortada text-lg-right text-center font-italic mx-auto">
-          <h1 class=" titulo font-italic">La calle habla</h1>
-          <p class="subtitulo  mt-3  ml-lg-4 font-italic ">
-            ¡Todo un viaje por la ciudad de experiencias interminables!
-          </p>
-          <h4>AGENCIA DE CONTENIDOS CULTURALES</h4>
-          <div class="lineaT">
-            <img
-              src="<?php echo get_theme_file_uri('assets//images/icon/Linea2-01.png') ?>"
-              alt="Linea"
-              title="Linea"
-            >
-          </div>
-        </div>
+        <!-- <span class="eventoMovil-subTitle">Valencia - Venezuela</span> -->
       </div>
-    </section>
-    <!-- FIN DE MENU ADENTRO -->
-    <!-- nuevo menu-->
-    <section class="navegacion"></section>
-    <nav class="navegacion seccion container-fluid 	row  flex-column align-items-center font-italic align-content-xs-center justify-content-center m-0">
-      <div class="menu">
-        <div class="menu-naveg">
-          <div class="menu-naveg-link">
-            <a href="#manifiesto" class="ancla">
-              <h2 class="menu-naveg-link-text">MANIFIESTO</h2>
-            </a>
-          </div>
-        </div>
-        <div class="menu-naveg">
-          <div class="menu-naveg-link">
-            <a href="#orguiaCultural" class="ancla">
-              <h2 class="menu-naveg-link-text">ORGÍA CULTURAL</h2>
-            </a>
-          </div>
-        </div>
-        <div class="menu-naveg">
-          <div class="menu-naveg-link">
-            <a href="#caminemos" class="ancla"> <h2 class="menu-naveg-link-text">CAMINEMOS</h2> </a>
-          </div>
-        </div>
-        <div class="menu-naveg">
-          <div class="menu-naveg-link">
-            <a href="#contacto" class="ancla"> <h2 class="menu-naveg-link-text">CONTACTO</h2> </a>
-          </div>
-        </div>
-        <div class="botonClose"></div>
+    <?php 
+        $i++;
+      } ?>
+    <?php if($homepagePosts->post_count > 0 && $i === $homepagePosts->post_count) { ?>
       </div>
-    </nav>
-    <!-- FIN MENU AFUERA -->
-    <!-- iniciode la infografia -->
-    <!-- Manifiesto -->
-    <section id="manifiesto" class="manifiesto ">
-      <div class="manifiesto-titulo container-fulid d-flex flex-column align-items-center  justify-content-center bg-light">
-        <img
-        src="<?php echo get_theme_file_uri('assets/images/icon/Separador.png') ?>"
-        alt="Sombra"
-        class="sombra"
-        title="Sombra"
-        />
-        <h2>Manifiesto</h2>
-        <img
-        src="<?php echo get_theme_file_uri('assets/images/icon/Separador.png') ?>"
-        alt="Sombra"
-        class="sombra"
-        title="Sombra"
-        />
+      <div class="show-boton moreEvents">
+        <span style="color: #fff !important">VER MÁS</span>
       </div>
-      <div class="manifiesto-img"></div>
-      <div class="bgRayas manifiesto-texto font-italic">
-        <div class="manifiesto-cont-p">
-          <?php
-          the_content(); // Manifiesto dinámico!
-          ?>
-        </div>
-      </div>
-      <div class="manifiesto-footer">
-        <div class="manifiesto-footer-redes">
-          <a href="https://www.instagram.com/transeuntehabla/"> <i class="fa fa-instagram"></i> </a>
-          <a href="https://www.facebook.com/transeuntehabla/"> <i class="fa fa-facebook-f"></i> </a>
-          <a href="https://www.linkedin.com/company/transeuntehabla/"> <i class="fa fa-linkedin-in"></i> </a>
-        </div>
-        <div class="manifiesto-footer-redes">
-          <a href="https://transeuntehabla.com/public/Manifiesto.pdf"> <i class="fa fa-download" aria-hidden="true"></i> </a>
-        </div>
-      </div>
-    </section>
-    <!-- FIN Manifiesto -->
-
-    <!-- seccion orgia cultural -->
-    <section id="orguiaCultural" class="orgiaCultural">
-      <div class="orgiaCultural-img"></div>
-      <div class="orgiaCultural-eventos">
-        <?php
-        $homepagePosts = new WP_Query(array(
-          'post_type' => array('evento', 'contenido'),
-          'orderby' => 'date',
-        ));
-        while($homepagePosts->have_posts()) { $homepagePosts->the_post(); ?>
-        <div class="evento">
-          <div class="eventoimg">
-            <?php
-            the_post_thumbnail('post-thumbnail', array(
-              'title' => get_the_title(get_post_thumbnail_id(get_the_id()))
-              ));
-            ?>
-          </div>
-          <div class="evento-title font-italic">
-            <span class="mx-auto"> <?php echo get_the_title(); ?> </span>
-            <div class="evento-title-verMas btn btn-light">
-              <span>Ver Más</span> <i class="fa fa-arrow-up evento-arrow" aria-hidden="true"></i>
-            </div>
-          </div>
-          <div class="evento-content">
-            <div class="evento-content-text">
-              <h2 class="evento-content-text-title"><?php echo get_the_title(); ?></h2>
-              <p class="evento-content-text-p"> <?php echo get_the_excerpt(); ?> </p>
-              <p class="evento-content-text-info">
-              <?php if(get_post_type() === 'evento') { ?>
-                <span><? echo get_field('lugar') ?></span>
-                <span><? echo get_field('fecha') ?></span>
-                <span><? echo get_field('tipo_de_evento') ?></span>
-                <span><? echo get_field('tipo_de_espacio') ?></span>
-                <span>Asistentes: <? echo get_field('numero_de_asistentes') ?>+</span>
-              <?php } ?>
-              <?php if(get_post_type() === 'contenido') { ?>
-                <span><? echo get_field('plataforma_o_red_social') ?></span>
-                <span>Alcance: <? echo get_field('alcance') ?></span>
-              <?php } ?>
-              </p>
-              <a class="evento-content-text-link btn btn-dark w-100 font-italic " href="<?php echo get_permalink(); ?>">
-                Ir a evento
-              </a>
-            </div>
-          </div>
-        </div>
-        <?php } wp_reset_postdata(); ?>
-      </div>
-      <!-- Eventos responsive -->
-      <div class="orgiaCultural-eventosMovil">
-      <?php
-        $homepagePosts = new WP_Query(array(
-          'post_type' => array('evento', 'contenido'),
-          'orderby' => 'date',
-        ));
-        $i = 0;
-        while($homepagePosts->have_posts()) {
-          $homepagePosts->the_post();
-          if($i === 4) { ?>
-          <div class="colapsable orgiaCultural-eventosMovil-C">
-      <?php } ?>
-        <div class="eventoMovil">
-          <a class="wow rotateInDownLeft  " href="<?php echo get_permalink(); ?>"
-            ><span class="eventoMovil-subTitle"><?php echo get_the_title(); ?></span></a
-          >
-          <!-- <span class="eventoMovil-subTitle">Valencia - Venezuela</span> -->
-        </div>
-      <?php 
-          $i++;
-        } ?>
-      <?php if($homepagePosts->post_count > 0 && $i === $homepagePosts->post_count) { ?>
-        </div>
-        <div class="show-boton moreEvents">
-          <span style="color: #fff !important">VER MÁS</span>
-        </div>
-      <?php } wp_reset_postdata();
-      ?>
-      </div>
-    </section>
+    <?php } wp_reset_postdata();
+    ?>
+    </div>
+  </section>
     <!-- FIN seccion orgia cultural -->
     <!-- seccion caminemos y proyectos, que hacemos? -->
     <section id="caminemos" class="caminemos container-fluid">
--- themes/transeunte-theme/header.php
+++ themes/transeunte-theme/header.php
@@ -2,12 +2,26 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!--meta seo-->
-    <meta name="description" content="#">
-    <meta name="author" content="Johandry Loyo">
-    <meta name="keywords" content="culture, periodism, people, photography, design">
-    <!--fin de meta seo-->
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+    >
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
+    <header>
+      <a href="<?php echo site_url(); ?>">
+        <img
+          src="<?php echo get_theme_file_uri('assets/images/logo-medium.png') ?>"
+          alt="Transeúnte Logo"
+        >
+      </a>
+      <nav>
+        <ul>
+          <li><a href="#">¿Qué hacemos?</a></li>
+          <li><a href="#">Orgía Cultural</a></li>
+          <li><a href="#">Transeúntes</a></li>
+        </ul>
+      </nav>
+      <a href="#">Contacto</a>
+    </header>
