e3acb9d40db576e69f5287fc71fe7f72d2a6ad82
Ali Padron <alijesus29@gmail.com>
2019-08-31 22:31:34 -0400

    Diseño de grid del blog terminado.

--- themes/transeunte-theme/index.php
+++ themes/transeunte-theme/index.php
@@ -14,19 +14,79 @@ get_header('blog');
 <?php
   if (have_posts()) {
 ?>
-  <section class="blog-posts">
-<?php        
-    while(have_posts()) {
-      the_post(); 
-?>
-    <article class="post">
-      <h2 class="post__title"><?php echo get_the_title(); ?></h2>
-      <p class="post__content"><?php echo get_the_content(); ?></p>
-    </article>
-<?php
-    }
-?>
-  </section>
+  <div class="wrapper">
+    <div class="row row--gutters-large">
+      <div class="row__large-10">
+        <section class="blog-posts">
+      <?php
+        $postCounter = 0;        
+        while (have_posts()) {
+          the_post();
+          if ($postCounter) {
+            if ($postCounter === 1) {
+              echo '<div class="blog-posts__others">';
+              echo '<div class="row">';
+            }
+      ?>
+              <div class="row__large-4">
+                <article class="post">
+                  <?php the_post_thumbnail('thumbnail'); ?>
+                  <h2 class="post__title"><?php echo get_the_title(); ?></h2>
+                  <p class="post__content"><?php echo wp_trim_words(get_the_content()); ?></p>
+                </article>
+              </div>           
+      <?php
+            if ($postCounter + 1 === $wp_query->post_count) {
+              echo '</div>';
+              echo '</div>';
+            }
+          } else {
+      ?>
+          <article class="blog-posts__most-recent">
+            <div class="row">
+              <div class="row__large-8">
+                <?php the_post_thumbnail('thumbnail'); ?>
+              </div>
+              <div class="row__large-4">
+                <h2 class="post__title"><?php echo get_the_title(); ?></h2>
+                <p class="post__content"><?php echo wp_trim_words(get_the_content()); ?></p>
+              </div>
+            </div>
+          </article>
+      <?php
+          }
+        $postCounter++;
+        }
+      ?>
+        </section>
+      </div>
+      <div class="row__large-2">
+        <section class="featured-posts">
+      <?php
+        $featuredPosts = new WP_Query(array(
+          'posts_per_page' => 1,
+          'post_type' => 'post',
+        ));
+        while ($featuredPosts->have_posts()) {
+          $featuredPosts->the_post();
+      ?>
+          <article class="featured-post">
+            <?php the_post_thumbnail('thumbnail'); ?>
+            <h2 class="featured-post__title">
+              <?php echo get_the_title(); ?>
+            </h2>
+            <p class="featured-post__content">
+              <?php echo wp_trim_words(get_the_content(), 18); ?>
+            </p>
+          </article>
+      <?php
+        }
+      ?>
+        </section>
+      </div>
+      
+    </div>
+  </div>
 <?php
   }
 ?>
