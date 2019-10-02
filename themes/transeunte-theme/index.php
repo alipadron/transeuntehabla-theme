617fac05b1b5432ea6cc45d1f6c446de8dda8e47
Alí Padrón <alijesus29@gmail.com>
2019-10-02 19:58:43 -0400

    Se pueden colocar más de 4 posts en el blog home.

--- themes/transeunte-theme/index.php
+++ themes/transeunte-theme/index.php
@@ -33,44 +33,52 @@ get_header('blog');
           if ($postCounter) {
             if ($postCounter === 1) { // Otros posts
               echo '<div class="blog-posts__others">';
+              
+            }
+
+            if (($postCounter - 1) % 3 === 0) {
               echo '<div class="row row--gutters-large row--equal-height-at-large">';
             }
       ?>
-              <div class="row__large-4">
-                <article class="blog-posts__others__post">
-                  <picture>
-                    <source
-                      media="(min-width: 992px)"
-                      srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeSmall'); ?>"
-                    >
-                    <source
-                      media="(min-width: 768px)"
-                      srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeMedium'); ?>"
-                    >
-                    <source
-                      media="(min-width: 576px)"
-                      srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeSmall'); ?>"
-                    >
-                    <img
-                      alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>"
-                      title="<?php echo get_the_title(get_post_thumbnail_id()); ?>"
-                      class="blog-posts__others__post__image"
-                      src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeExtraSmall'); ?>"
-                    >
-                  </picture>
-                  <div class="wrapper wrapper--keep-padding generic-content-container">
-                    <h2 class="blog-posts__others__post__title">
-                      <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-                    </h2>
-                    <p class="blog-posts__others__post__content">
-                      <?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?>
-                    </p>
-                  </div>
-                </article>
-              </div>           
+            <div class="row__large-4">
+              <article class="blog-posts__others__post">
+                <picture>
+                  <source
+                    media="(min-width: 992px)"
+                    srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeSmall'); ?>"
+                  >
+                  <source
+                    media="(min-width: 768px)"
+                    srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeMedium'); ?>"
+                  >
+                  <source
+                    media="(min-width: 576px)"
+                    srcset="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeSmall'); ?>"
+                  >
+                  <img
+                    alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true); ?>"
+                    title="<?php echo get_the_title(get_post_thumbnail_id()); ?>"
+                    class="blog-posts__others__post__image"
+                    src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'postLandscapeExtraSmall'); ?>"
+                  >
+                </picture>
+                <div class="wrapper wrapper--keep-padding generic-content-container">
+                  <h2 class="blog-posts__others__post__title">
+                    <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                  </h2>
+                  <p class="blog-posts__others__post__content">
+                    <?php echo has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?>
+                  </p>
+                </div>
+              </article>
+            </div>
       <?php
-            if ($postCounter + 1 === $wp_query->post_count) {
+            if ($postCounter % 3 === 0 || $postCounter + 1 === $wp_query->post_count) {
               echo '</div>';
+            }
+
+            if ($postCounter + 1 === $wp_query->post_count) {
+              // echo '</div>';
               echo '</div>';
             }
           } else { // Post principal
