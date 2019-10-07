4f9dc80e82e05a6612df17367a2fc625e49501ec
Alí Padrón <alijesus29@gmail.com>
2019-10-07 15:45:34 -0400

    Se agrega el formulario de contacto.

--- themes/transeunte-theme/front-page.php
+++ themes/transeunte-theme/front-page.php
@@ -225,7 +225,7 @@ while(have_posts()) { the_post();
   <div class="modal__inner">
     <div class="wrapper">
       <div class="row row--gutters-small">
-        <div class="row__large-4">
+        <div class="row__large-6">
           <div class="modal__text">
             <p class="modal__text--large modal__text--white modal__text--bold modal__text--narrow modal__text--italic">"Queremos unirnos con otros que andan por allí en un caminar constante".</p>
             <p class="modal__text--red">¡Encontrémonos!</p>
@@ -234,9 +234,10 @@ while(have_posts()) { the_post();
               +58 424 463 2964 <br>
               info@transeuntehabla.com</p>
           </div>
-          
         </div>
-        <div class="row__large-8"></div>
+        <div class="row__large-6">
+          <?php echo do_shortcode('[contact-form-7 id="1339" title="Contact form 1"]'); ?>
+        </div>
       </div>
       
     </div>
