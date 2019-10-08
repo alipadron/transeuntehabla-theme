8c5cda6c98b2e4521150dd15e68661a9fcf0a9e2
Alí Padrón <alijesus29@gmail.com>
2019-10-08 16:20:47 -0400

    Archive parte 1.

---
+++ themes/transeunte-theme/archive.php
@@ -0,0 +1,47 @@
+<?php
+get_header('blog');
+?>
+<main class="main-blog">
+<?php get_template_part('template-parts/content', 'banner'); ?>
+<div class="archive">
+  <div class="wrapper wrapper--no-padding-until-large">
+    <div class="archive__title-container">
+      <h1 class="archive__title-container__text">
+        <?php echo explode(' ', get_the_archive_title())[1]; ?>
+      </h1>
+      <div class="archive__title-container__lines">
+        <div class="archive__title-container__lines__left"></div>
+        <div class="archive__title-container__lines__center"></div>
+        <div class="archive__title-container__lines__right"></div>
+      </div>
+    </div>
+    <?php
+      $postCounter = 0;
+      if (have_posts()) {
+        echo '<div class="blog-posts__others">';
+        while(have_posts()) {
+          the_post();
+          if ($postCounter % 3 === 0) {
+            echo '<div class="row row--gutters-large row--equal-height-at-large">';
+          }
+          get_template_part('template-parts/content', 'blog-post');
+          if (($postCounter + 1) % 3 === 0 || $postCounter + 1 === $wp_query->post_count) {
+            echo '</div>';
+          }
+          $postCounter++;
+        }
+        echo '</div>';
+      }
+    ?>
+  <div class="archive__pagination">
+    <?php echo paginate_links(); ?>
+  </div>
+  </div>
+</div>
+
+
+<?php get_template_part('template-parts/content', 'social-icons'); ?>
+</main>
+
+
+<?php get_footer(); ?>
